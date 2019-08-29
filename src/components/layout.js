27aef531637711baee475ceeb9cdcfaff62a7bfa
Michael Johnson <mb.johnsonbece@gmail.com>
2019-08-29 09:59:52 -0500

    Project startup

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import "../css/global.css"
 
 import Header from "./header"
 import "./layout.css"
@@ -25,16 +26,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+      <div className="container mx-auto">
+        {/* <header>{header}</header> */}
+        {/* <main>{children}</main> */}
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
